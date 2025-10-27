8ab1cf56278eae189a13e055489efddd0efa200b
MatthewFuchs <82055493+MatthewFuchs@users.noreply.github.com>
2025-10-27 02:29:36 -0700

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -278,7 +278,7 @@ export default function HomePage() {
                 </span>
                 <div>
                   <div className="text-2xl md:text-3xl font-bold text-gray-900">
-                    Fast Resonse
+                    Fast Response
                   </div>
                   <p className="text-sm text-gray-600">
                     We respond fast and promptly
